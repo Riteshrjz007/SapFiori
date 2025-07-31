defac394aa78099af42ae34a2456ee9c60dacbe6
Ritesh Kumar <riteshrjz007@gmail.com>
2025-07-31 09:48:59 +0000

    Initial Commit

---
+++ sap_basic_template/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sapbasictemplate/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ sap_basic_template/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapbasictemplate/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
